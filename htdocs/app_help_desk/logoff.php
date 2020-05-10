73b11b68558094ca800f3c264544daed2ea16b46
UeberteG <46915513+UeberteG@users.noreply.github.com>
2020-05-09 21:09:18 -0300

    Update logoff.php

--- htdocs/app_help_desk/logoff.php
+++ htdocs/app_help_desk/logoff.php
@@ -3,27 +3,4 @@
 
 	session_destroy();
 	header('Location: index.php');
-	/*
-	echo "<pre>";
-	print_r($_SESSION);
-	echo "</pre>"; 
-	//remover índices do array de sessão
-	//unset()
-
-	unset($_SESSION['x']); //para remover o índice apenas se existir
-
-	echo "<pre>";
-	print_r($_SESSION);
-	echo "</pre>"; 
-
-	//destruir a variável de sessão
-	//session_destroy()
-
-	session_destroy(); //será destruída
-	//forçar um redirecionamento
-
-	echo "<pre>";
-	print_r($_SESSION);
-	echo "</pre>"; 
-	*/
 ?>
